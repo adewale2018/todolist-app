d182c6e9a6d3996d5ecf4fe84e6b830506ebda0d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-02-24 10:09:34 +0100

    Add todo component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "../App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h2>TodoList App</h2>
     </div>
   );

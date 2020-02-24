d8c13f1145b70ce99beec5657e8d49a154ac30ad
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-02-24 11:12:10 +0100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav style={{marginBottom: '15px'}}>
+      <div className='nav-wrapper'>
+        <a href='/' className='brand-logo'>
+          Todo App
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='badges.html'>About</a>
+          </li>
+          <li>
+            <a href='collapsible.html'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;

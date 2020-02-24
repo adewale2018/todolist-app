06a33692a8753fc4d5a033426fc1cef7a7c5db32
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-02-24 12:50:45 +0100

    Implement delete functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,11 @@ import React, { Component } from "react";
 class Todo extends Component {
   constructor(props) {
     super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete(id) {
+    this.props.deleteTodo(this.props.id);
   }
   render() {
     return (
@@ -15,6 +20,7 @@ class Todo extends Component {
           <button
             className='waves-effect waves-light btn deep-orange lighten-1'
             style={{ marginLeft: "5px" }}
+            onClick={this.handleDelete}
           >
             <i className='large material-icons'>delete</i>
           </button>
